f4893366cb0bd671cd0f918a1ad5969fdac556fc
arnoldyoo <airnold0986@gmail.com>
2019-04-22 17:14:50 +0900

    aaaaa

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,12 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Watch } from 'vue-property-decorator';
 import Menu from './layouts/menu.vue';
 import Contents from './layouts/contents.vue';
 import Footer from './layouts/footer.vue';
 import SideBar from './layouts/side-bar.vue';
+import {Getter} from 'vuex-class';
 
 @Component({
   components: {
@@ -27,19 +28,30 @@ import SideBar from './layouts/side-bar.vue';
   },
 })
 export default class App extends Vue {
+  @Getter('sideBar') sideBar !: any;
 
   mounted() {
     // 모든 state 변경을 감지하기 때문에 좋아보이지 않음..
-    this.$store.subscribeAction((action, state) => {
-      if (action.type === 'setSideBarStatus') {
-        const el: any = this.$refs['mainContainer'];
-        if (action.payload) {
-          el.style.marginLeft = '250px';
-        } else {
-          el.style.marginLeft = '0';
-        }
-      }
-    })
+    // this.$store.subscribeAction((action, state) => {
+    //   if (action.type === 'setSideBarStatus') {
+    //     const el: any = this.$refs['mainContainer'];
+    //     if (action.payload) {
+    //       el.style.marginLeft = '250px';
+    //     } else {
+    //       el.style.marginLeft = '0';
+    //     }
+    //   }
+    // })
+  }
+
+  @Watch('sideBar')
+  sideBarStatusChange(currentValue: boolean, oldValue: boolean) {
+    const el: any = this.$refs['mainContainer'];
+    if (currentValue) {
+      el.style.marginLeft = '250px';
+    } else {
+      el.style.marginLeft = '0';
+    }
   }
 }
 </script>
--- src/layouts/side-bar.vue
+++ src/layouts/side-bar.vue
@@ -6,26 +6,37 @@
 </template>
 
 <script lang="ts">
-    import { Component, Prop, Vue } from 'vue-property-decorator';
-    import {Action} from 'vuex-class';
+    import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+    import {Action, Getter} from 'vuex-class';
 
     @Component
     export default class SideBar extends Vue {
         @Action('setSideBarStatus') setSideBarStatus: any;
+        @Getter('sideBar') sideBar !: any;
 
         mounted() {
             // 모든 state 변경을 감지하기 때문에 좋아보이지 않음..
-            this.$store.subscribeAction((action, state) => {
-              if (action.type === 'setSideBarStatus') {
-                  const el: any = this.$refs['sidebar'];
-                  if (action.payload) {
-                      el.style.width = '250px';
-                  } else {
-                      // close
-                      el.style.width = '0';
-                  }
-              }
-            })
+            // this.$store.subscribeAction((action, state) => {
+            //   if (action.type === 'setSideBarStatus') {
+            //       const el: any = this.$refs['sidebar'];
+            //       if (action.payload) {
+            //           el.style.width = '250px';
+            //       } else {
+            //           // close
+            //           el.style.width = '0';
+            //       }
+            //   }
+            // })
+        }
+
+        @Watch('sideBar')
+        sideBarStatusChange(currentValue: boolean, oldValue: boolean) {
+            const el: any = this.$refs['sidebar'];
+            if (currentValue) {
+                el.style.width = '250px';
+            } else {
+                el.style.width = '0';
+            }
         }
 
         private closeSideNav() {
